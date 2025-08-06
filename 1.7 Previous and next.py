1834f06ee5743404fc6c19b6a78d508467511f8a
Abhinav P <poyililabhinav@gmail.com>
2025-08-06 22:07:42 +0530

    Add files via upload

---
+++ 1.7 Previous and next.py
@@ -0,0 +1,8 @@
+a=int(input())
+an=a+1
+ap=a-1
+an=str(an)
+ap=str(ap)
+a=str(a)
+print('The next number for the number '+a+' is '+an+'.')
+print('The previous number for the number '+a+' is '+ap+'.')
